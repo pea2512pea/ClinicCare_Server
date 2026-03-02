d90885b8c5ac6fbb0373e1c48a3c9e1daf5674cd
Suppakorn Sirikueakunchai <s6706022510174@email.kmutnb.ac.th>
2026-03-02 23:04:48 +0700

    refactor: update package type to module and remove unused route views

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,10 @@ import {
 } from "../middlewares/protectRoute.js";
 
 import { verifyToken } from "../middlewares/verifyToken.js";
-import adminViews from "./admin/views.js";
 import adminRoutes from "./admin/adminRoutes.js";
 import authenRoutes from "./authenRoutes.js";
 import doctorStaffRoutes from "./doctor/doctorRoutes.js";
-import doctorViews from "./doctor/views.js";
 import userRoutes from "./user/userRoutes.js";
-import userViews from "./user/views.js";
 
 export default (app) => {
   app.get("/", protectUnauthenticatedView, (req, res) => {
@@ -33,7 +30,4 @@ export default (app) => {
   app.use("/api/user", verifyToken, userRoutes);
   app.use("/api/doctor", verifyToken, doctorStaffRoutes);
   app.use("/api/admin", verifyToken, adminRoutes);
-  app.use("/admin", protectAdminView, adminViews);
-  app.use("/doctor", protectDoctorView, doctorViews);
-  app.use("/user", protectUserView, userViews);
 };
